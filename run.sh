89e2d139a1195581502a73525e0b4f985c60c7b1
MaxBubblegum47 <lorenzostigliano@gmail.com>
2022-11-09 23:44:47 +0100

    Updating the bash runner

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+echo "Check if Python 3.x version is available and PyMongo is installed..."
 
-python Python/demo.py
+if [[ "$(python -V)" =~ "Python 3" ]] || [[ "$(python3 -V)" =~ "Python 3" ]] && python -c "import pymongo" &> /dev/null ; then
+
+    echo "Launching the Python/Demo.py"
+    python Python/demo.py
+
+else
+
+    echo "Is not possible to launche the demo."
+
+fi
